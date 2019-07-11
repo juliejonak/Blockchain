be4fda138502ca3b86c24e2a36e3206dc0b1ef1b
Julie Jonak <juliejonak10@gmail.com>
2019-07-11 17:33:10 -0500

    Creates my_id file and mining_id

--- credit_for_mining_p/blockchain.py
+++ credit_for_mining_p/blockchain.py
@@ -136,7 +136,7 @@ class Blockchain(object):
         """
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:6] == "000000"
+        return guess_hash[:5] == "00000"
 
     def valid_chain(self, chain):
         """
